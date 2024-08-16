a11d3d755bcdb1c3d36515330fe1afe627ee7956
Samuel Spink <dr.greenjet@gmail.com>
2024-08-16 20:41:20 +0200

    feat: github workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "", // <=== here it is
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
